7ebac53b2a0abd5793a2991bc7930a9f45413729
mussaratshamsher <musaratskhan@github.com>
2025-01-28 20:27:14 +0500

    auth

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware,createRouteMatcher } from '@clerk/nextjs/server'
 
-const isPrivateRoute = createRouteMatcher(['/dashboard'])
+const isPrivateRoute = createRouteMatcher(['/dashboard(.*)'])
 
 export default clerkMiddleware(async (auth, request) => {
   if (!isPrivateRoute(request)) {
